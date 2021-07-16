c40d14366e21b5c3887dbe2cf7df682988cd1886
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-16 15:48:41 +0530

    Added: Linked List by appending 56, 30 & 70

--- Linked_List/src/Linked_List/Linked_List.java
+++ Linked_List/src/Linked_List/Linked_List.java
@@ -3,17 +3,14 @@ package Linked_List;
 public class Linked_List {
 
 	public static void main(String[] args) {
-		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(70);
-		myFirstNode.setNext(mySecondNode);
-		mySecondNode.setNext(myThirdNode);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
 		MyLinkedList myLinkedList = new MyLinkedList();
-		myLinkedList.append(myFirstNode);
-		myLinkedList.append(mySecondNode);
-		myLinkedList.append(myThirdNode);
-		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
 		System.out.println(result);
 	}
-
 }
